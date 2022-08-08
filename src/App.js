6aa945914f0256e73a9bdecbb1b6a6d6f1b64b0c
= <=>
2022-08-08 15:39:19 +0100

    calculator progress

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import { useState } from 'react';
 
 function App() {
+  const [result, setResult] = useState(0);
+  const numpad = [1,2,3,4,5,6,7,8,9]
+  const nowTime = () => {
+    let d = new Date()
+    let now = Date.now()
+    let hr = d.getHours()
+    let min = d.getMinutes()
+    return `${hr}:${min}`
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="calculator">
+        <header className="App-header">
+          { nowTime() }
+        </header>
+
+        <div className="calculator-body">
+          <div className="result">{result}</div>
+
+          <div className="numpads">
+            {
+              numpad.map((num, i)=> (
+                <button key={i} className="num pad">{num}</button>
+              ))
+            }
+          </div>
+        </div>
+      </div>
+
     </div>
   );
 }
